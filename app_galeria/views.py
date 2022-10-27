5d9177e7c3ddf8da3c659bf88a716627c778b9ff
Conrado <conrado07@gmail.com>
2022-10-27 10:41:07 -0300

    Commit 3

--- app_galeria/views.py
+++ app_galeria/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(req):
-    return HttpResponse('Hello Wolrd')
+    return render(req, 'galeria/index.html')
+
+def imagem(req):
+    return render(req, 'galeria/imagem.html')
